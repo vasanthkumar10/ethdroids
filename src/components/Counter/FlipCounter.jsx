46c3429020763e211c4805ce7d1d37e51ae78dfb
sivasaithota <65367120+sivasaithota@users.noreply.github.com>
2022-04-01 22:05:47 +0530

    Fix date

--- src/components/Counter/FlipCounter.jsx
+++ src/components/Counter/FlipCounter.jsx
@@ -14,7 +14,7 @@ function FlipCounter() {
           minuteTitle="Minutes"
           secondTitle="Seconds"
           size={"small"}
-          endAt={"2022-04-01 16:35:00"} // Date/Time
+          endAt={"2022-04-10 16:35:00"} // Date/Time
         />
       </span>
       <span className="desktop">
