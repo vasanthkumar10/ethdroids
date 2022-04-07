bcd5571d644070513eede7388f21ea5ef76abdf7
VASANTHKUMAR V <vasizebron10@gmail.com>
2022-04-08 01:57:14 +0530

    change browse router to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React  from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Main from './Main'
 import Wallet from './components/Sections/Wallet';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/wallet" element={<Wallet />} />
         <Route path="/*" element={<Main />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
